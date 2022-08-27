1ffc20a4cb781d48c8c5c62447530f8aa3ca7fee
rohit <rhtry.tech@gmail.com>
2022-08-28 02:11:21 +0530

    Build app structure

---
+++ src/Components/index.js
@@ -0,0 +1,13 @@
+//Common file for exporting all the different componenrts 
+
+export { default as Navbar } from './Navbar'; 
+
+export { default as Feed } from './Feed'; 
+
+export { default as VideoDetail } from './VideoDetail'; 
+
+export { default as SearchFeed } from './SearchFeed'; 
+
+export { default as ChannelDetail } from './ChannelDetail'; 
+
+
