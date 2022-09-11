c61c2f8be1462bf48058421f4f82fa994ca1722c
rohit <rhtry.tech@gmail.com>
2022-09-11 13:09:29 +0530

    Update ...

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,65 +1,87 @@
-// Feed Component 
-// Conatins={ 
-//   LeftSide = Category Panel 
+// Feed Component
+// Conatins={
+//   LeftSide = Category Panel
 //   RightSide = Video Conatainer/ Video Feed
 // }
 
-import React from 'react'
+import React from "react";
 
 //Importing Hooks
 import { useState, useEffect } from "react";
 
 //Importing MUI Components
-import { Box, Stack, Typography } from '@mui/material'; 
+import { Box, Stack, Typography } from "@mui/material";
 
-//Importing Sidebar Component 
-import { Sidebar, Videos } from '../Components';
+//Importing Sidebar Component
+import { Sidebar, Videos } from "../Components";
 
 //Importing API call module
-import { fetchFromAPI } from '../Utilities/fetchFromAPI';
+import { fetchFromAPI } from "../Utilities/fetchFromAPI";
 
 const Feed = () => {
-
   // Using UseState to initialize the 'selectedCategory for now
-  const [selectedCategory, setSelectedCategory] = useState('New'); 
-  const [ videos, setVideos ] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
 
   // Using a UseEffect hook to display api data as soon as the component loads
   useEffect(() => {
     // setVideos(null);
 
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-
-    //exctracting data
-      .then((data) => setVideos(data.items))
-    }, [selectedCategory]);
+      //exctracting data
+      .then((data) => setVideos(data.items));
+  }, [selectedCategory]);
 
   return (
     // Stack will be wrapping the entire feed section both LeftSide and RightSide
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" }}}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       {/* sx=byDefault > coulmn 
       md=mediumDevices > row */}
 
-      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} /> 
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
-      {/* Typography component is used for all kinds of text feilds like <h>,<p> */}
-      <Typography classname="copyright"
-      variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
-        Copyright Text
-      </Typography>
+        {/* Typography component is used for all kinds of text feilds like <h>,<p> */}
+        <Typography
+          classname="copyright"
+          variant="h7"
+          sx={{ mt: 1.8, color: "#fff" }}
+        >
+          Youtube Clone
+        </Typography>
+        <br />
+        <Typography
+          classname="copyright"
+          variant="h7"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Rohit Roy | 2022
+        </Typography>
       </Box>
 
-      <Box p={2} sc={{ overflowY: 'auto', height: '90vh', flex: 2}}>
-      <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}> 
-        { selectedCategory } <span style={{ color: '#F31503' }}>videos</span>
-      </Typography>
+      <Box p={2} sc={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
 
-      <Videos videos={videos} />
+        <Videos videos={videos} />
       </Box>
-
     </Stack>
-  )
-}
+  );
+};
 
 export default Feed;
